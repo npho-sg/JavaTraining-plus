b861b8c295cf47e9523b81d8d519cdccef6defa8
npho-sg <ho-nhat-phi@s-giken.com>
2026-08-06 15:05:43 +0000

    google認証対応、トークン生成

--- webapp/src/main/java/com/s_giken/training/webapp/authentication/Authentication.java
+++ webapp/src/main/java/com/s_giken/training/webapp/authentication/Authentication.java
@@ -1,11 +1,25 @@
 package com.s_giken.training.webapp.authentication;
 
+import java.security.SecureRandom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Authentication {
 
     public static void main(String[] args) {
 
         String username = args[0];
-        String gmail = args[1];
+        String password = args[1];
+        String gmail = args[2];
+        SecureRandom random = new SecureRandom();
+        int token = random.nextInt(1_000_000);
+
+        ApplicationContext context = SpringApplication.run(Authentication.class, args);
+        UserAuthRepository repository = context.getBean(UserAuthRepository.class);
+        repository.addToken(username, password, gmail, token);
 
     }
 
--- webapp/src/main/java/com/s_giken/training/webapp/authentication/UserAuthRepository.java
+++ webapp/src/main/java/com/s_giken/training/webapp/authentication/UserAuthRepository.java
@@ -12,10 +12,17 @@ public class UserAuthRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // gmailログイン
     public boolean searchGmailuser(String gmail) {
         String sql = "SELECT EXISTS (SELECT 1 FROM T_USER WHERE gmail = ?)";
         boolean result = jdbcTemplate.queryForObject(sql, Boolean.class, gmail);
         return result;
+    }
+
+    // gmailユーザー一時登録
+    public void addToken(String username, String password, String gmail, int token) {
+        String sql = "INSERT INTO t_mailuser_add(username, passwprd, gmail, token)" + "VALUES(?, ?, ?, ?)";
+        jdbcTemplate.update(sql, username, password, gmail, token);
     }
 
 }
